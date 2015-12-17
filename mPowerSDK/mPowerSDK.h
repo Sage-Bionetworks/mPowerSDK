48cd4d3ad5148b8312eac44b824a7e51d72b2563
Shannon Young <shannon.young@sagebase.org>
2015-12-17 13:03:46 -0800

    Created mPowerSDK project for open source app

---
+++ mPowerSDK/mPowerSDK.h
@@ -0,0 +1,19 @@
+//
+//  mPowerSDK.h
+//  mPowerSDK
+//
+//  Created by Shannon Young on 12/17/15.
+//  Copyright © 2015 Sage Bionetworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mPowerSDK.
+FOUNDATION_EXPORT double mPowerSDKVersionNumber;
+
+//! Project version string for mPowerSDK.
+FOUNDATION_EXPORT const unsigned char mPowerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mPowerSDK/PublicHeader.h>
+
+
