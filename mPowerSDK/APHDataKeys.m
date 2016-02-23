57d5ac468b178a69281aad0dd1f4af175e229832
Jake Krog <jake@hellothread.com>
2016-02-23 14:22:05 -0800

    Update tremor task identifier

--- mPowerSDK/APHDataKeys.m
+++ mPowerSDK/APHDataKeys.m
@@ -36,11 +36,11 @@ NSString *const APHWalkingActivitySurveyIdentifier              = @"4-APHTimedWa
 NSString *const APHVoiceActivitySurveyIdentifier                = @"3-APHPhonation-C614A231-A7B7-4173-BDC8-098309354292";
 NSString *const APHTappingActivitySurveyIdentifier              = @"2-APHIntervalTapping-7259AC18-D711-47A6-ADBD-6CFCECDED1DF";
 NSString *const APHMemoryActivitySurveyIdentifier               = @"7-APHSpatialSpanMemory-4A04F3D0-AC05-11E4-AB27-0800200C9A66";
-NSString *const APHTremorActivitySurveyIdentifier               = @"0-APHTremorAssessment-00000000-0000-0000-0000-000000000000"; // TODO: update identifier
 NSString *const APHEnrollmentSurveyIdentifier                   = @"1-EnrollmentSurvey-20EF83D2-E461-4C20-9024-F43FCAAAF4C3";
 NSString *const APHMyThoughtsSurveyIdentifier                   = @"8-MyThoughts-12ffde40-1551-4b48-aae2-8fef38d61b61";
 NSString *const APHFeedbackSurveyIdentifier                     = @"9-Feedback-394348ce-ca4f-4abe-b97e-fedbfd7ffb8e";
 NSString *const APHMedicationTrackerSurveyIdentifier            = @"1-APHMedicationTracker-20EF8ED2-E461-4C20-9024-F43FCAAAF4C3";
+NSString *const APHTremorActivitySurveyIdentifier               = @"1-APHTremor-108E189F-4B5B-48DC-BFD7-FA6796EEf439";
 
 NSString *const APHPhonationScoreSummaryOfRecordsKey            = @"ScoreSummaryOfRecords";
 NSString *const APHRightSummaryNumberOfRecordsKey               = @"SummaryNumberOfRecords";
