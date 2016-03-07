ba378f8c47871b1c84214368973933bfac7068a4
Jake Krog <jake@hellothread.com>
2016-03-06 16:13:41 -0800

    Add APHCircleView to project

---
+++ mPowerSDK/Dashboard/APHCircleView.h
@@ -0,0 +1,13 @@
+//
+//  APHCircleView.h
+//  mPowerSDK
+//
+//  Created by Jake Krog on 2016-03-06.
+//  Copyright © 2016 Sage Bionetworks. All rights reserved.
+//
+
+#import <APCAppCore/APCAppCore.h>
+
+@interface APHCircleView : APCCircleView
+
+@end
