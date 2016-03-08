784c836c25db8481864a11a2469e07aa859b2477
Everest Liu <everest@hellothread.com>
2016-03-08 14:56:37 -0800

    PAN-127 - Remove "Index Of" from expanded correlation graph

--- mPowerSDK/Dashboard/APHTableViewDashboardGraphItem.m
+++ mPowerSDK/Dashboard/APHTableViewDashboardGraphItem.m
@@ -11,38 +11,35 @@
 
 @implementation APHTableViewDashboardGraphItem
 
-+(NSAttributedString *)legendForSeries1:(NSString *)series1
-                                series2:(NSString *)series2
-						colorForSeries1:(UIColor *)color1
-						colorForSeries2:(UIColor *)color2
-{
-    NSAssert(series1 != nil, @"Pass a valid series 1 name");
-    
-    UIFont *font = [UIFont appLightFontWithSize:14.0f];
-    UIColor *red = [UIColor appTertiaryRedColor];
-    UIColor *yellow = [UIColor appTertiaryYellowColor];
-    UIColor *darkGray = [UIColor darkGrayColor];
-    
-    NSAttributedString *indexOf = [[NSAttributedString alloc]initWithString:NSLocalizedStringWithDefaultValue(@"Index of", @"APCAppCore", APCBundle(), @"Index of", nil) attributes:@{NSFontAttributeName : font, NSForegroundColorAttributeName : darkGray }];
-    NSAttributedString *s1 = [[NSMutableAttributedString alloc]initWithString:series1 attributes:@{NSFontAttributeName : font, NSForegroundColorAttributeName : color1 ?: red }];
-    NSAttributedString *space = [[NSAttributedString alloc]initWithString:@" "];
-    NSAttributedString *versus = [[NSAttributedString alloc]initWithString:NSLocalizedStringWithDefaultValue(@"vs", @"APCAppCore", APCBundle(), @"vs", nil) attributes:@{NSFontAttributeName : font, NSForegroundColorAttributeName : darkGray }];
-    
-    NSMutableAttributedString *legend = [[NSMutableAttributedString alloc]initWithAttributedString:indexOf];
-    [legend appendAttributedString:space];
-    [legend appendAttributedString:s1];
-    [legend appendAttributedString:space];
-    [legend appendAttributedString:versus];
-    [legend appendAttributedString:space];
-    
-    if (series2) {
-        NSAttributedString *s2 = [[NSMutableAttributedString alloc]initWithString:series2 attributes:@{NSFontAttributeName : font, NSForegroundColorAttributeName : color2 ?: yellow}];
-        
-        [legend appendAttributedString:s2];
-    }
-    
-    return legend;
-    
++ (NSAttributedString *)legendForSeries1:(NSString *)series1
+								 series2:(NSString *)series2
+						 colorForSeries1:(UIColor *)color1
+						 colorForSeries2:(UIColor *)color2 {
+	NSAssert(series1 != nil, @"Pass a valid series 1 name");
+
+	UIFont *font = [UIFont appLightFontWithSize:14.0f];
+	UIColor *red = [UIColor appTertiaryRedColor];
+	UIColor *yellow = [UIColor appTertiaryYellowColor];
+	UIColor *darkGray = [UIColor darkGrayColor];
+
+	NSAttributedString *s1 = [[NSMutableAttributedString alloc] initWithString:series1
+																	attributes:@{NSFontAttributeName : font, NSForegroundColorAttributeName : color1 ?: red}];
+	NSAttributedString *space = [[NSAttributedString alloc] initWithString:@" "];
+	NSAttributedString *versus = [[NSAttributedString alloc] initWithString:NSLocalizedStringWithDefaultValue(@"vs", @"APCAppCore", APCBundle(), @"vs", nil)
+																 attributes:@{NSFontAttributeName : font, NSForegroundColorAttributeName : darkGray}];
+
+	NSMutableAttributedString *legend = [[NSMutableAttributedString alloc] initWithAttributedString:space];
+	[legend appendAttributedString:s1];
+	[legend appendAttributedString:space];
+	[legend appendAttributedString:versus];
+	[legend appendAttributedString:space];
+
+	if (series2) {
+		NSAttributedString *s2 = [[NSMutableAttributedString alloc] initWithString:series2 attributes:@{NSFontAttributeName : font, NSForegroundColorAttributeName : color2 ?: yellow}];
+		[legend appendAttributedString:s2];
+	}
+
+	return legend;
 }
 
 @end
