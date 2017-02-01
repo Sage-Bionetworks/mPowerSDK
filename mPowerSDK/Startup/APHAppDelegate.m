eed98dd4c2c878492f88e9c933f5cd77d710bdb4
Josh Bruhin <josh@saviapps.com>
2017-02-01 13:49:47 -0700

    Remove kPrivacyPolicyUrlKey

--- mPowerSDK/Startup/APHAppDelegate.m
+++ mPowerSDK/Startup/APHAppDelegate.m
@@ -54,7 +54,6 @@ static NSString *const kJsonScheduleStringKey           = @"scheduleString";
 static NSString *const kJsonTasksKey                    = @"tasks";
 static NSString *const kJsonScheduleTaskIDKey           = @"taskID";
 static NSString *const kJsonSchedulesKey                = @"schedules";
-static NSString *const kPrivacyPolicyUrlKey             = @"privacyPolicyUrlString";
 
 static NSString *const kAppStoreLink                    = @"https://appsto.re/us/GxN85.i";
 
